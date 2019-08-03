1eb52b052f60ca9a5ba185f4e00261a8e2206eac
Alex Colombo <uricaine@hotmail.com>
2019-08-03 11:58:55 +0200

    creating routes

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 // Requires
 let express = require('express');
+let port = 3000;
 
 // Inicializar express
 let app = express();
-let port = 3000;
+
+// Rutas
+app.get('/:id', (req, res, next) => {
+  res.json({ message: `Hola ${req}` });
+});
 
 // Escuchar peticiones
 app.listen(port, () => {
