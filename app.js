87dfe943c77c006cc93a71dbabde7e52585f631e
Alex Colombo <uricaine@hotmail.com>
2019-08-03 12:08:00 +0200

    init routes

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ let port = 3000;
 let app = express();
 
 // Rutas
-app.get('/:id', (req, res, next) => {
-  res.status(200).json({
-    ok: true,
-    message: `Hola ${req.params.id}`
-  });
+app.get('/:id?', (req, res, next) => {
+  if (req.params.id === undefined) {
+    res.status(200).json({
+      ok: true,
+      message: `Hola ${'Sin nombre'}`
+    });
+  } else {
+    res.status(200).json({
+      ok: true,
+      message: `Hola ${req.params.id}`
+    });
+  }
 });
 
 // Escuchar peticiones
