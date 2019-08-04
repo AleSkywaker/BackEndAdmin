8584922e3dd519376248f0c2f3135b43a4c54104
Alex Colombo <uricaine@hotmail.com>
2019-08-04 21:16:56 +0200

    connect to database

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 // Requires
 let express = require('express');
+const mongoose = require('mongoose');
 let port = 3000;
 
 // Inicializar express
 let app = express();
+mongoose.connect('mongodb://localhost:27017/hospitalDB', { useNewUrlParser: true });
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  // we're connected!
+  console.log('Conectatos a la base de datos');
+});
 
 // Rutas
 app.get('/:id?', (req, res, next) => {
