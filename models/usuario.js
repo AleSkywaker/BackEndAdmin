97c9f52990bec1908507f053b902b4ac35a6f189
Alex Colombo <uricaine@hotmail.com>
2019-08-04 21:39:04 +0200

    define schema for user

--- models/usuario.js
+++ models/usuario.js
@@ -2,4 +2,10 @@ const mongoose = require('mongoose');
 
 let Schema = mongoose.Schema;
 
-let usuarioSchema = new Schema({});
+let usuarioSchema = new Schema({
+  nombre: { type: String, required: [true, 'El nombre es necesario'] },
+  email: { type: String, unique: true, required: [true, 'El correo es necesario'] },
+  password: { type: String, required: [true, 'La contraseña es necesaria'] },
+  img: { type: String, required: false },
+  role: { type: String, required: true, default: 'USER_ROLE' }
+});
