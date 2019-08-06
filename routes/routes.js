f6a633fb9e4715fb3c844a0c18f5d8ecaba68069
Alex Colombo <uricaine@hotmail.com>
2019-08-06 20:27:13 +0200

    new files

---
+++ routes/routes.js
@@ -0,0 +1,13 @@
+app.get('/:id?', (req, res, next) => {
+  if (req.params.id === undefined) {
+    res.status(200).json({
+      ok: true,
+      message: `Hola ${'Sin nombre'}`
+    });
+  } else {
+    res.status(200).json({
+      ok: true,
+      message: `Hola ${req.params.id}`
+    });
+  }
+});
