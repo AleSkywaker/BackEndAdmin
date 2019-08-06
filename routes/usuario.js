8657f0b62f172eda1e3c3f476ecde66fcb2054e8
Alex Colombo <uricaine@hotmail.com>
2019-08-06 20:40:35 +0200

    acceding to data base

--- routes/usuario.js
+++ routes/usuario.js
@@ -3,17 +3,20 @@ const app = express();
 const Usuario = require('../models/usuario');
 
 app.get('/', (req, res, next) => {
-  if (req.params.id === undefined) {
-    res.status(200).json({
-      ok: true,
-      message: `Hola ${'USUARIO'}`
-    });
-  } else {
-    res.status(200).json({
-      ok: true,
-      message: `Hola ${req.params.id}`
-    });
-  }
+  Usuario.find({}, (err, usuarios) => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        message: `Error en base de datos`,
+        errors: err
+      });
+    } else {
+      res.status(200).json({
+        ok: true,
+        usuarios
+      });
+    }
+  });
 });
 
 module.exports = app;
