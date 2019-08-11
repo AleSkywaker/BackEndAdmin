bdc3e93d913588ccffd71ad0e427f802a6fec8e4
Alex Colombo <uricaine@hotmail.com>
2019-08-11 10:09:41 +0200

    creating login

--- routes/login.js
+++ routes/login.js
@@ -5,10 +5,20 @@ const Usuario = require('../models/usuario');
 
 app.post('/', (req, res, next) => {
   let body = req.body;
-  res.status(200).json({
-    ok: true,
-    body,
-    mensaje: { message: 'Login correcto' }
+
+  Usuario.findOne({ email: body.email }, (err, usuarioDB) => {
+    if (err) {
+      return res.status(403).json({
+        ok: false,
+        mensaje: 'No se encontró usuario',
+        errors: err
+      });
+    }
+    res.status(200).json({
+      ok: true,
+      usuarioDB,
+      mensaje: { message: 'Login correcto' }
+    });
   });
 });
 
