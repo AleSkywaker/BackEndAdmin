f7a558c6d586962f64d0bb38fb3fed7940237964
Alex Colombo <uricaine@hotmail.com>
2019-08-11 10:06:38 +0200

    creating login

--- routes/login.js
+++ routes/login.js
@@ -4,8 +4,10 @@ const app = express();
 const Usuario = require('../models/usuario');
 
 app.post('/', (req, res, next) => {
+  let body = req.body;
   res.status(200).json({
     ok: true,
+    body,
     mensaje: { message: 'Login correcto' }
   });
 });
