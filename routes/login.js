36ff0c7d37c7b70ce1001c3b795c227be10b7e92
Alex Colombo <uricaine@hotmail.com>
2019-08-11 10:02:23 +0200

    new route

--- routes/login.js
+++ routes/login.js
@@ -3,6 +3,11 @@ const bcrypt = require('bcryptjs');
 const app = express();
 const Usuario = require('../models/usuario');
 
-app.post('/', (req, res, next) => {});
+app.post('/', (req, res, next) => {
+  res.status(200).json({
+    ok: true,
+    mensaje: { message: 'Login correcto' }
+  });
+});
 
 module.exports = app;
