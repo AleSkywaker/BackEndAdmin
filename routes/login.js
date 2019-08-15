271bf68dc3826e8650eec8677a364e0cc8570788
Alex Colombo <uricaine@hotmail.com>
2019-08-15 10:22:48 +0200

    middlewares

--- routes/login.js
+++ routes/login.js
@@ -4,6 +4,7 @@ var jwt = require('jsonwebtoken');
 const app = express();
 const Usuario = require('../models/usuario');
 
+const SEED = require('../config/config').SEED;
 app.post('/', (req, res, next) => {
   let body = req.body;
 
@@ -32,7 +33,7 @@ app.post('/', (req, res, next) => {
 
     // Crear Token
     usuarioDB.password = ';)';
-    let token = jwt.sign({ usuario: usuarioDB }, 'esto-es-la-semill@', { expiresIn: 14400 }); // 4horas
+    let token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400 }); // 4horas
     res.status(200).json({
       ok: true,
       usuarioDB,
