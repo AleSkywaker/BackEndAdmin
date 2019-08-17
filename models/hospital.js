fe03c5fee619aa7e3e25454013e66557bfa4c427
Alex Colombo <uricaine@hotmail.com>
2019-08-17 11:28:02 +0200

    hospital model created

--- models/hospital.js
+++ models/hospital.js
@@ -3,7 +3,7 @@ var Schema = mongoose.Schema;
 
 var hospitalSchema = new Schema(
   {
-    nombre: { type: String, required: [true, 'El	nombre	es	necesario'] },
+    nombre: { type: String, required: [true, 'El	nombre del hospital	es	necesario'] },
     img: { type: String, required: false },
     usuario: { type: Schema.Types.ObjectId, ref: 'Usuario' }
   },
