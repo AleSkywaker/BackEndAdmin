0fe9afa7843faee492897e297955cbde85b30612
Alex Colombo <uricaine@hotmail.com>
2019-08-29 10:33:10 +0200

    new route

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -1,10 +1,20 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res, next) => {
-	res.status(200).json({
-		ok: true,
-		message: 'todo ok'
+const Hospital = require('../models/hospital');
+
+app.get('/todo/:busqueda', (req, res, next) => {
+	let busqueda = req.params.busqueda;
+
+	Hospital.find({ nombre: busqueda }, (err, hospitales) => {
+		if (err) {
+			console.log('error', err);
+		} else {
+			res.status(200).json({
+				ok: true,
+				hospitales
+			});
+		}
 	});
 });
 
