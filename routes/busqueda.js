01d4b8653546953f277b53c82cc08e89d2a83637
Alex Colombo <uricaine@hotmail.com>
2019-08-29 10:53:45 +0200

    new route

--- routes/busqueda.js
+++ routes/busqueda.js
@@ -5,36 +5,26 @@ const Hospital = require('../models/hospital');
 
 app.get('/todo/:busqueda', (req, res, next) => {
 	let busqueda = req.params.busqueda;
-
 	let regex = new RegExp(busqueda, 'i');
 
-	Hospital.find({ nombre: regex }, (err, hospitales) => {
-		if (err) {
-			console.log('error', err);
-		} else {
-			res.status(200).json({
-				ok: true,
-				hospitales
-			});
-		}
+	buscarHospitales(regex).then(hospitales => {
+		res.status(200).json({
+			ok: true,
+			hospitales
+		});
 	});
 });
 
-function buscarHospital(busqueda, regex) {
-
-  return new Promise((resolve, reject)=>{
-
-    Hospital.find({ nombre: busqueda }, (err, hospitales) => {
-
-      if(err){
-        
-      }
-
-    });
-
-
-  })
-
+function buscarHospitales(regex) {
+	return new Promise((resolve, reject) => {
+		Hospital.find({ nombre: regex }, (err, hospitales) => {
+			if (err) {
+				reject('Error al cargar hospitales', err);
+			} else {
+				resolve(hospitales);
+			}
+		});
+	});
 }
 
 module.exports = app;
