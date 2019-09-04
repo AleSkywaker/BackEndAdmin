67fd08a9e857c1adc83e58cc054ee9b4946c2659
Alex Colombo <uricaine@hotmail.com>
2019-09-04 17:24:06 +0200

    new file for new route

---
+++ routes/upload.js
@@ -0,0 +1,7 @@
+// Requires
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+// Inicializar express
+const app = express();
+const port = 3000;
