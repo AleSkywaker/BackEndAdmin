2039f398608776168618f2c1753230a04e0cef78
Alex Colombo <uricaine@hotmail.com>
2019-09-05 11:41:33 +0200

    added express fileupload

--- routes/upload.js
+++ routes/upload.js
@@ -1,12 +1,22 @@
 // Requires
 const express = require('express');
+const fileUpload = require('express-fileupload');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 // Inicializar express
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res, next) => {
+// default options
+app.use(fileUpload());
+
+app.put('/', (req, res, next) => {
+  if (!req.files) {
+    return res.status(400).json({
+      ok: false,
+      message: 'No ha subido imagen'
+    });
+  }
   res.status(200).json({
     ok: true,
     message: 'Nueva ruta upload'
