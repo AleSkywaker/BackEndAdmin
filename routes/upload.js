1e6d51758c8b2bfbf3d9e87b83952ba724ea9abe
Alex Colombo <uricaine@hotmail.com>
2019-09-06 12:38:11 +0200

    (done) upload image

--- routes/upload.js
+++ routes/upload.js
@@ -1,26 +1,33 @@
 // Requires
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
+
 // Inicializar express
 const app = express();
-const port = 3000;
 
 // default options
 app.use(fileUpload());
 
 app.put('/:tipo/:id', (req, res, next) => {
+  // Obtener nombre del archivo
+  let imagen = req.files.img;
+  let nombreCortado = imagen.name.split('.');
+  let extension = nombreCortado[nombreCortado.length - 1];
+
+  // Extensiones validas
+  let extensionesValidas = ['png', 'jpg', 'gif', 'jpeg'];
   let tipo = req.params.tipo;
   let id = req.params.id;
-
+  // Tipos validos
   let tiposValidos = ['hospitales', 'medicos', 'usuarios'];
 
   if (tiposValidos.indexOf(tipo) < 0) {
     return res.status(400).json({
       ok: false,
       message: 'Tipo no valido',
-      errors: { message: `Los tipos validos son ${tiposValidos.join(', ')}` }
+      errors: {
+        message: `Los tipos validos son ${tiposValidos.join(', ')}`
+      }
     });
   }
 
@@ -31,33 +38,36 @@ app.put('/:tipo/:id', (req, res, next) => {
     });
   }
 
-  //Obtener nombre del archivo
-  let nombreImagen = req.files.img;
-  let nombreCortado = nombreImagen.name.split('.');
-  let extension = nombreCortado[nombreCortado.length - 1];
-
-  //Extensiones validas
-  let extensionesValidas = ['png', 'jpg', 'gif', 'jpeg'];
-
   if (extensionesValidas.indexOf(extension) < 0) {
     return res.status(400).json({
       ok: false,
       message: 'Extension no valida',
-      errors: { message: `Las extensiones validas son ${extensionesValidas.join(', ')}` }
+      errors: {
+        message: `Las extensiones validas son ${extensionesValidas.join(', ')}`
+      }
     });
   }
 
   // Nombre del archivo personalizado
-
-  let nombreArchivo = `${id}-${new Date().getMilliseconds()}.${extension}`;
+  let nombreImagenPersonalizado = `${id}-${new Date().getMilliseconds()}.${extension}`;
 
   // Mover el archivo
-  let path = `./uploads/${tipo}/${nombreArchivo}`;
-  res.status(200).json({
-    ok: true,
-    message: 'Imagen subida correctamente!!',
-    extension,
-    nombreArchivo
+  let path = `./uploads/${tipo}/${nombreImagenPersonalizado}`;
+
+  imagen.mv(path, err => {
+    if (err) {
+      return res.status(500).json({
+        ok: false,
+        message: 'Error al mover la imagen',
+        errors: err
+      });
+    }
+    res.status(200).json({
+      ok: true,
+      message: 'Imagen guardada correctamente!!',
+      extension,
+      nombreImagenPersonalizado
+    });
   });
 });
 
