824b791f0aaa2c03da204960b1f8eb932ba17098
Alex Colombo <uricaine@hotmail.com>
2019-09-19 23:37:40 +0200

    new route to serve image

--- routes/imagenes.js
+++ routes/imagenes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', (req, res, next) => {
   res.status(200).json({
     ok: true,
     message: 'aqui esta tu imagen'
