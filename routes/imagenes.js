989be827fbec74b884ba70654e5008dd99cbb15c
Alex Colombo <uricaine@hotmail.com>
2019-09-21 18:33:10 +0200

    creating new route to show images

--- routes/imagenes.js
+++ routes/imagenes.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/:tipo/:img', (req, res, next) => {
+  let tipo = req.params.tipo;
   res.status(200).json({
     ok: true,
     message: 'aqui esta tu imagen'
